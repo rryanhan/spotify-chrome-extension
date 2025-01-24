21c96eff8fca03602f2f8a7d85cfcf7962b0a70a
ryanhan <ryanjhan10@gmail.com>
2025-01-23 16:32:54 -0800

    chrome.tabs.create

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const loginButton = document.getElementById('loginButton');
 if (loginButton) {
   loginButton.addEventListener("click", () => {
     console.log('button clicked');
-    window.location.href = "http://localhost:3000/login";
+    chrome.tabs.create({ url: "http://localhost:3000/login" });
   });
 }
 
